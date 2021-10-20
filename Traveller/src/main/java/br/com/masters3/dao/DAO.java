f0936f6aad1c8ba8495e34dc2d69560b094f29a8
Isabella Moretti <isabella.moretti@outlook.com>
2021-10-19 21:13:37 -0300

    primeiro commit

---
+++ Traveller/src/main/java/br/com/masters3/dao/DAO.java
@@ -0,0 +1,10 @@
+package br.com.masters3.dao;
+
+public interface DAO <T extends Object> 
+{
+	public void  create (T Object);
+	public T read(T object);
+	public void update(T object);
+	public void delete (T object);
+
+}
